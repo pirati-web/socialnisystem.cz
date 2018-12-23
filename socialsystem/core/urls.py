cd95165cc39fb7a4862226306dba14605f32710c
xaralis <filip.varecha@fragaria.cz>
2018-12-23 11:04:49 +0100

    [feature] Homepage as flatpage

--- socialsystem/core/urls.py
+++ socialsystem/core/urls.py
@@ -8,7 +8,6 @@ from . import views
 app_name = 'core'
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='core/home.html')),
     path('davky/<int:pk>-<str:slug>/', DetailView.as_view(model=models.Benefit, template_name='core/benefit_detail.html'), name='benefit-detail'),
     path('davky/prehled/', views.BenefitOverview.as_view(), name='benefit-overview'),
     path('davky/na-co-mam-narok/', views.BenefitClaimView.as_view(), name='benefit-claim'),
