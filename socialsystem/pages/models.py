6abb241797e664202e7d5ecfa1cc7035dfbc513b
xaralis <filip.varecha@fragaria.cz>
2019-06-02 12:07:27 +0200

    Improve SEO for homepage

--- socialsystem/pages/models.py
+++ socialsystem/pages/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class FlatPageMeta(models.Model):
     flatpage = models.OneToOneField(to='flatpages.FlatPage', on_delete=models.CASCADE, verbose_name='Stránka', related_name='metadata')
+    title = models.CharField('Titulek', max_length=255, blank=True)
     keywords = models.CharField('Klíčová slova', help_text='Oddělujte čárkou', max_length=150, blank=True)
     description = models.TextField('Krátký popis', blank=True)
 
