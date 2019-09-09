01d1e8ea869373d5400064c53007994fd3b74bde
xaralis <filip.varecha@fragaria.cz>
2019-09-09 15:14:25 +0200

    Add redir from /kalkulacka and claim help text

--- socialsystem/pages/urls.py
+++ socialsystem/pages/urls.py
@@ -1,8 +1,10 @@
 from django.contrib.flatpages.views import flatpage
 from django.urls import path
+from django.views.generic.base import RedirectView
 
 app_name = 'pages'
 
 urlpatterns = [
+    path('kalkulacka', RedirectView.as_view(url='/nase-reseni/rodicovsky-prispevek/')),
     path('<path:url>', flatpage),
 ]
